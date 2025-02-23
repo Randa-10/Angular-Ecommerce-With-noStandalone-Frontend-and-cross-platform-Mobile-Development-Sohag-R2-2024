b8af4141d1e97e8271bc645acbe5089b5103489a
Randa10 <randa010011012@gmail.com>
2025-02-23 12:40:42 +0200

    Day4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:ProductsComponent},
+  //lazy loading
+  {path:'user',loadChildren:()=>import('./components/user/user.module').then(m=>m.UserModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
